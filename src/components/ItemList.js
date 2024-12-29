86b7919cb4d6bdc0c19f746d3d85ff6521687b98
yekaditya11 <yekaditya11@gmail.com>
2024-12-29 22:50:16 +0530

    update2

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const items = [
-  { id: 1, name: "Item 1", description: "This is the description for Item 1" },
-  { id: 2, name: "Item 2", description: "This is the description for Item 2" },
-  { id: 3, name: "Item 3", description: "This is the description for Item 3" },
+  { id: 1, name: "Item 1", description: "Description for Item 1" },
+  { id: 2, name: "Item 2", description: "Description for Item 2" },
+  { id: 3, name: "Item 3", description: "Description for Item 3" },
 ];
 
 const ItemList = () => {
